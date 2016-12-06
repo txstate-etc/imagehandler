691de87453daaf61c718aede23fb2f4985b6ce63
wickning1 <git@mythgaming.net>
2016-12-06 14:29:13 -0600

    fix problem with cleanup task

--- cmd.sh
+++ cmd.sh
@@ -12,7 +12,7 @@ HTTPDPID=$!
 
 LAST_CLEANUP=$(date +%s)
 while true; do
-	if [ $(date +%H%M) == "0900" ] && [ $(date -d "-1hour" +%s) -ge LAST_CLEANUP ]; then
+	if [ $(date +%H%M) == "0900" ] && [ $(date -d "-1hour" +%s) -ge $LAST_CLEANUP ]; then
 		LAST_CLEANUP=$(date +%s)
 		find /var/cache/resize -name data -delete -mtime +${IMAGEHANDLERCLEANUPDAYS-60}
 	fi
