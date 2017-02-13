df0bb568834b9911dd5300b4c4aed293a17538d3
wickning1 <git@mythgaming.net>
2017-02-13 16:54:43 -0600

    run cache cleanup as www-data user in case cache directory is an NFS mount with root squash

--- cmd.sh
+++ cmd.sh
@@ -14,7 +14,7 @@ LAST_CLEANUP=$(date +%s)
 while true; do
 	if [ $(date +%H%M) == "0900" ] && [ $(date -d "-1hour" +%s) -ge $LAST_CLEANUP ]; then
 		LAST_CLEANUP=$(date +%s)
-		find /var/cache/resize -name data -delete -mtime +${IMAGEHANDLERCLEANUPDAYS-60}
+		su -c "find /var/cache/resize -name data -delete -mtime +${IMAGEHANDLERCLEANUPDAYS-60}" www-data
 	fi
 	sleep 1
 done
