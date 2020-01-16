4d117a444fa97196e58ae60213aa146d9e4fba11
Nick Wing <nwing@txstate.edu>
2020-01-16 10:52:03 -0600

    more aggressive cache cleanup

--- cmd.sh
+++ cmd.sh
@@ -14,7 +14,8 @@ LAST_CLEANUP=$(date +%s)
 while true; do
 	if [ $(date +%H%M) == "0900" ] && [ $(date -d "-1hour" +%s) -ge $LAST_CLEANUP ]; then
 		LAST_CLEANUP=$(date +%s)
-		su -c "find /var/cache/resize -name data -delete -mtime +${IMAGEHANDLERCLEANUPDAYS-60}" www-data
+		su -c "find /var/cache/resize -delete -mtime +${IMAGEHANDLERCLEANUPDAYS-60}" www-data
+		su -c "find /var/cache/resize -delete -empty -type d" www-data
 	fi
 	sleep 1
 done
